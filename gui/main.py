12723081aa8f92e40a5aa98f87084f6054a38607
christian k <christiankkwok910@gmail.com>
2026-01-05 23:57:49 -0800

    colors!!

--- gui/main.py
+++ gui/main.py
@@ -12,11 +12,14 @@ def main() -> None:
     layout = QHBoxLayout(central)
 
     left_panel = QLabel("left_panel")
+    left_panel.setStyleSheet("background: #191919; color: white;")
     center_panel = QLabel("center_panel")
+    center_panel.setStyleSheet("background: #121212; color: white;")
     right_panel = QLabel("right_panel")
+    right_panel.setStyleSheet("background: #191919; color: white;")
 
     layout.addWidget(left_panel)
-    layout.addWidget(center_panel)
+    layout.addWidget(center_panel, 1)
     layout.addWidget(right_panel)
 
     window.setCentralWidget(central)
