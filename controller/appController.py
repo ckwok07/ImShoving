a6ef5f43cafe4ce874acc6e26c6740f8ca80608f
christian k <christiankkwok910@gmail.com>
2026-01-07 00:02:29 -0800

    updated hande_action method in controller

--- controller/appController.py
+++ controller/appController.py
@@ -6,5 +6,18 @@ class AppController:
         self.state = state
     
     def handle_action(self, action: Action) -> None:
-        print(f"state = {self.state}")
-        print(f"action = {action}")
+        print("state before:", self.state)
+
+        if action.name == "CHECK":
+            pass
+
+        elif action.name == "CALL":
+            pass
+
+        elif action.name == "RAISE" and action.size is not None:
+            self.state.pot *= action.size
+
+        elif action.name == "ALL_IN":
+            print("ALL IN clicked")
+
+        print("state after:", self.state)
