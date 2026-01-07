4f5bdb1822cc1ab774193ba91c26fb857e71d860
christian k <christiankkwok910@gmail.com>
2026-01-06 23:23:55 -0800

    added color changing hover on buttons

--- gui/actions/action_button.py
+++ gui/actions/action_button.py
@@ -4,4 +4,12 @@ class ActionButton(QPushButton):
     def __init__(self, label: str) -> None:
         super().__init__(label)
 
-        self.setStyleSheet("background: #2b2b2b; font-size: 14px")
+        self.setStyleSheet("""
+                           QPushButton {
+                                background: 2b2b2b; 
+                                font-size: 14px; 
+                                color: white;
+                           }
+                           QPushButton:hover {
+                                background-color: #3a3a3a
+                           }""")
