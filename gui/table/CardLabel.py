041fe84217a8911844b3abdbfd1ce5a576719092
christian k <christiankkwok910@gmail.com>
2026-01-15 03:05:11 -0800

    added images to cards !!

---
+++ gui/table/CardLabel.py
@@ -0,0 +1,43 @@
+from PyQt6.QtWidgets import QLabel
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
+
+from main.model.Card import Card
+from main.model.Rank import Rank
+from main.model.Suit import Suit
+
+class CardLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setStyleSheet("""QLabel {background-color: #2b2b2b;
+                           border-radius: 6px;}""")
+        
+        self.setFixedSize(65, 100)
+        
+        self.card = None
+        self.face_up = True
+        
+    def set_card(self, card: Card):
+        self.card = card
+        self.face_up = True
+        self.update()
+
+    def hide_card(self):
+        self.face_up = False
+        self.setPixmap(QPixmap())
+
+    def clear(self):
+        self.card = None
+        self.setPixmap(QPixmap())
+
+    def update(self):
+        if not self.card:
+            self.clear()
+            return
+        
+        filename = f"assets/cards/image.png"
+        self.setPixmap(QPixmap(filename))
+
--- gui/table/HeroHand.py
+++ gui/table/HeroHand.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLabel
 from PyQt6.QtCore import Qt
+from .CardLabel import CardLabel
 from main.model.Card import Card
 
 class HeroHand(QWidget):
@@ -9,30 +10,22 @@ class HeroHand(QWidget):
         self.mainLayout = QHBoxLayout(self)
         self.mainLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.cards: list[QLabel] = []
+        self.cards = []
 
         for i in range(2):
-            label = QLabel("")
-            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            label.setStyleSheet("""QLabel { background-color: #2b2b2b;
-                                color: white;
-                                font-size: 22px;
-                                border-radius: 6px;
-                                padding: 10px;
-                                min-width: 40px;
-                                min-height: 70px; }""")
-            self.cards.append(label)
-            self.mainLayout.addWidget(label)
+            cardLabel = CardLabel()
+            self.cards.append(cardLabel)
+            self.mainLayout.addWidget(cardLabel)
         
     def set_hand(self, cards: list[Card] | None) -> None:
-        for label in self.cards:
-            label.setText("")
+        for cardLabel in self.cards:
+            cardLabel.clear()
 
         if not cards:
             return
 
-        for label, card in zip(self.cards, cards):
-            label.setText(str(card))
+        for cardLabel, card in zip(self.cards, cards):
+            cardLabel.set_card(card)
     
     def set_active(self, active: bool):
         if active:
